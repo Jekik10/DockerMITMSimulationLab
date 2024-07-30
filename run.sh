2953cf5f6149ce6518572f7b61e02c9eea9bc199
jekik <jekik77@gmail.com>
2024-07-30 22:20:41 +0200

    Stable circa ma con i ping duplicati su ids

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#controlla che il primo parametro sia un file esistente
+if ! [ -f "$1" ]; then
+    echo "File $1 does not exist."
+    exit 1
+fi
+#copia il contenuto del file in suricata.rules
+cp $1 suricata.rules
+
+sudo modprobe ipt_TEE
+docker compose up --build -d
+
+rm suricata.rules
