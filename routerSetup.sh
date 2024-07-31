bf98f340a56b969193531f291667acc1520b28cc
jekik <jekik77@gmail.com>
2024-07-31 12:54:32 +0200

    FIX: package mirroring use tc and not tee anymore

--- routerSetup.sh
+++ routerSetup.sh
@@ -3,9 +3,6 @@
 # Abilita l'inoltro IP
 echo 1 > /proc/sys/net/ipv4/ip_forward 
 
-# Drop all ip rules
-iptables -F 
-
 # Regole di instradamento client to server
 iptables -t nat -A PREROUTING -d 172.20.0.3 -j DNAT --to-destination 172.21.0.2
 iptables -t nat -A POSTROUTING -s 172.20.0.0/24 -d 172.21.0.2 -j MASQUERADE
@@ -15,7 +12,10 @@ iptables -t nat -A PREROUTING -d 172.21.0.3 -j DNAT --to-destination 172.20.0.2
 iptables -t nat -A POSTROUTING -s 172.21.0.0/24 -d 172.20.0.2 -j MASQUERADE
 
 # Regole per clonare il traffico in arrivo da 172.20.0.2
-iptables -t mangle -A PREROUTING -s 172.20.0.2 -j TEE --gateway 172.22.0.2  
+tc qdisc add dev eth0 ingress
+tc filter add dev eth0 parent ffff: protocol ip u32 match ip src 172.20.0.2 action mirred egress mirror dev eth1
+
+
 
 # Mantieni il container in esecuzione
 exec "$@"
--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@
 #controlla che il primo parametro sia un file esistente
 if ! [ -f "$1" ]; then
     echo "File $1 does not exist."
+    echo "Required parameter: path-to-rules client-config server-config"
     exit 1
 fi
+
 #copia il contenuto del file in suricata.rules
 cp $1 suricata.rules
 
-# sudo modprobe ipt_TEE # oppure xt_TEE
 docker compose up --build -d
-
 rm suricata.rules
