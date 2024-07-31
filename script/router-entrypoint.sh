bfa32b394f6051caa16ce4df5d2969e4c7e365dd
jekik <jekik77@gmail.com>
2024-07-31 22:18:37 +0200

    fix: removed missplaced ip routes from router-entrypoint.sh

--- script/router-entrypoint.sh
+++ script/router-entrypoint.sh
@@ -3,9 +3,9 @@
 # Abilita l'inoltro IP
 echo 1 > /proc/sys/net/ipv4/ip_forward 
 
-# Configura le rotte statiche
-ip route add 172.21.0.0/24 via 172.20.0.3 dev eth0
-ip route add 172.20.0.0/24 via 172.21.0.3 dev eth2
+# # Configura le rotte statiche
+# ip route add 172.21.0.0/24 via 172.20.0.3 dev eth0
+# ip route add 172.20.0.0/24 via 172.21.0.3 dev eth2
 
 # Abilita il Masquerading
 iptables -t nat -A POSTROUTING -o eth2 -j MASQUERADE
