d46e6510a0b78e8a80510a1b5e917a588c1f9ee7
jekik <jekik77@gmail.com>
2024-08-10 12:17:07 +0200

    fix: ids now running on debian & commented nat masquerading

--- scripts/router-entrypoint.sh
+++ scripts/router-entrypoint.sh
@@ -4,8 +4,8 @@
 echo 1 > /proc/sys/net/ipv4/ip_forward 
 
 # Enable Masquerading
-iptables -t nat -A POSTROUTING -o eth2 -j MASQUERADE
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+# iptables -t nat -A POSTROUTING -o eth2 -j MASQUERADE
+# iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 # Rules to clone traffic from 172.20.0.2
 tc qdisc add dev eth0 ingress
@@ -13,3 +13,4 @@ tc filter add dev eth0 parent ffff: protocol ip u32 match ip src 172.20.0.2 acti
 
 # Keeps the container running
 exec "$@"
+  
